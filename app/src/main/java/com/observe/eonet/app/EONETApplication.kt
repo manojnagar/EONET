96873ffa01bfee230854e29888d3d7122e179f74
manoj <manoj@helpshift.comL>
2019-12-01 17:51:41 +0530

    Add multidex support

--- app/src/main/java/com/observe/eonet/app/EONETApplication.kt
+++ app/src/main/java/com/observe/eonet/app/EONETApplication.kt
@@ -1,12 +1,12 @@
 package com.observe.eonet.app
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 import com.observe.eonet.data.repository.DataRepository
 import com.observe.eonet.data.repository.DataSource
 import com.observe.eonet.util.schedulers.BaseSchedulerProvider
 import com.observe.eonet.util.schedulers.SchedulerProvider
 
-class EONETApplication : Application() {
+class EONETApplication : MultiDexApplication() {
 
     companion object {
         lateinit var dataSource: DataSource
